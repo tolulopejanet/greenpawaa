6c07d5a8e7d4f3149d18806425d37330607f1dd7
tolulope <tolulopefaleyejanet@gmail.com>
2025-05-22 08:01:43 +0100

    fourth update

--- src/routes/Aboutus.jsx
+++ src/routes/Aboutus.jsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Aboutus = () => {
   return (
     <div className="pt-28 px-6 sm:px-10 md:px-16 lg:px-24 pb-12 bg-white">
-      <h2 className="text-3xl md:text-4xl font-bold text-center mb-10 text-gray-800">
-        About Us
-      </h2>
+      <div className="text-center space-y-5">
+        <h2 className="text-3xl md:text-4xl font-bold text-gray-800">
+          About Us
+        </h2>
+        <p className="md:w-1/2 mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          accusantium eligendi debitis animi aspernatur quibusdam consequatur
+          delectus, nihil perferendis earum quod minima, natus esse odit
+          voluptate magnam unde itaque expedita?
+        </p>
+      </div>
+
+      <div className="py-12 md:w-4/5 mx-auto">
+        <Tabs>
+          <TabList>
+            <Tab>Our Mission</Tab>
+            <Tab>Our Vision</Tab>
+            <Tab>Support</Tab>
+          </TabList>
+
+          <TabPanel>
+            <h2>Any content 1</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
+      </div>
     </div>
   );
 };
